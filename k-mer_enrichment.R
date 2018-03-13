5d6e268711580164f75c346cc999b79e72f176b0
Carl de Boer <Carldeboer@users.noreply.github.com>
2018-03-13 17:03:39 -0400

    Update k-mer_enrichment.R
    adding doc and fixed bug

--- k-mer_enrichment.R
+++ k-mer_enrichment.R
@@ -1,6 +1,23 @@
 
 
 
+#' Find 
+#'
+#' Identifies which PCs distinguish a feature of interest. If the feature of interest contains two classes, performs a single rank sum test for each PC.
+#' If the feature of interest has >2 classes, performs a rank sum test to see if the PC separates the class from all others. 
+#' In either case, returns a data frame containing the PC, class, AUROC, and rank sum P-value for how well the PC identifies the class.
+#' 
+#' @param rotated A numeric matrix containing the k-mer loadings for however many PCs you want analyzed (rows are k-mers, columns are PCs).
+#' @param binaryKMerMatchesToTFs A matrix of k-mers by TF motifs, where each row is a k-mer and each column is a TF motif. Each entry in the matrix is 1 if the k-mer matches the TF's motif, and 0 otherwise.
+#' @param n_max The maximum number of k-mers to consider for each minimum hypergeometric test. Defaults to 3000. Should be no more than about 10% of the number of k-mers. 
+#' @return a data.frame containing the following columns: TF (the TF motif), PC (the PC), pLow (minimum hyper geometric p-value for low-weighted k-mers), kLow (the number of top k-mers that yielded maximal enrichment, for low-weighted k-mers), logORLow (the log odds ratio (observed/expected) of k-mers for the point of maximal enrichment among kowly-weighted k-mers), pHigh (as before, for high-scoring k-mers), kHigh (ibid), logORHigh (ibid), i (a unique integer for each PC-motif combination).
+#' @keywords 
+#' @export
+#' @examples
+#' kmerMat = inputKMerFreqs(sprintf("kMerFiles/%s.freq.gz",sampleDesc$id), IDs = sampleDesc$id)
+#' myPCA = doKMerPCA(kmerMat, nPCs = "jackstraw")
+#` treatmentPCs = findDistinguishingPCs(myPCA$x[,1:myPCA$nPCs], sampleDesc[c("id","treated")])
+
 getKMerTFEnrichment = function(rotated, binaryKMerMatchesToTFs,n_max=3000){
   z=1;
   rotated = rotated[row.names(binaryKMerMatchesToTFs),]; #filter out and sort rows so that they're in the same order
@@ -13,7 +30,7 @@ getKMerTFEnrichment = function(rotated, binaryKMerMatchesToTFs,n_max=3000){
         message(sprintf("\tTF = %i",tfi));
       }
       tfKMerEnrichments$TF[z] = colnames(binaryKMerMatchesToTFs)[tfi];
-      tfKMerEnrichments$PC[z] = names(rotated)[pci];
+      tfKMerEnrichments$PC[z] = colnames(rotated)[pci];
       curTest = minHG(binaryKMerMatchesToTFs[curOrder,tfi], n_max=n_max); #check for enrichment among low PC weights
       curTestRev = minHG(rev(binaryKMerMatchesToTFs[curOrder,tfi]), n_max=n_max); #check for enrichment among high PC weights
       
